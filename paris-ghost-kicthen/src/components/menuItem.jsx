ce9fb0bba1d140cd6e645bc55bd4a5013558e5dd
Deniz <denizhincal05@gmail.com>
2023-04-22 16:12:39 +0300

    Add Placeholder Menu Items to Menu Page

--- paris-ghost-kicthen/src/components/menuItem.jsx
+++ paris-ghost-kicthen/src/components/menuItem.jsx
@@ -6,15 +6,16 @@ export default function MenuItem({category, name, desc, price, img}) {
    
     return (
         <div className='flex flex-col gap-y-1 items-center text-center'>
-            <div className='relative w-[150px] h-[100px]'>
+            <div className='relative w-[100px] h-[100px]'>
                 <Image 
                     src={img}
                     fill
+                    className='object-contain'
                 />
             </div>
             <h3 className='font-semibold text-xl'>{name}</h3>
             <p className='font-light text-lg'>{desc}</p>
-            <h3 className='font-semibold text-xl'>{price}TL</h3>
+            <h3 className='font-semibold text-xl'>{price} €</h3>
         </div>
   )
 }
--- paris-ghost-kicthen/src/pages/index.jsx
+++ paris-ghost-kicthen/src/pages/index.jsx
@@ -18,12 +18,39 @@ export default function Home() {
         <h3 className='text-kitchen-green text-2xl font-semibold mt-10'>Bowls</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
-          <MenuItem img="/soup.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
-          <MenuItem img="/soup.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
-          <MenuItem img="/soup.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
-          <MenuItem img="/soup.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
+          <MenuItem img="/soup1.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
+          <MenuItem img="/soup1.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
+          <MenuItem img="/soup1.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
+          <MenuItem img="/soup1.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={120} category={"soup"}/>
         
-        </div>      
+         </div>      
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-10'>Bagels</h3>
+        <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
+        <div className='flex flex-col items-center mt-10 gap-y-10'>
+          <MenuItem img="/bagel.png" name="Bruschetta Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={50} category={"bagel"} />
+          <MenuItem img="/bagel.png" name="Bruschetta Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={50} category={"bagel"} />
+          <MenuItem img="/bagel.png" name="Bruschetta Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={50} category={"bagel"} />
+          <MenuItem img="/bagel.png" name="Bruschetta Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={50} category={"bagel"} />
+        
+        </div>
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-10'>Wraps</h3>
+        <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
+        <div className='flex flex-col items-center mt-10 gap-y-10'>
+          <MenuItem img="/wrap.png" name="Burrito Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={250} category={"wrap"} />
+          <MenuItem img="/wrap.png" name="Burrito Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={250} category={"wrap"} />
+          <MenuItem img="/wrap.png" name="Burrito Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={250} category={"wrap"} />
+          <MenuItem img="/wrap.png" name="Burrito Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={250} category={"wrap"} />
+                
+        </div>
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-10 text-center'>Something to Drink</h3>
+        <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
+        <div className='flex flex-col items-center mt-10 gap-y-10'>
+          <MenuItem img="/smoothie.png" name="Smoothie" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={30} category={"drink"} />
+          <MenuItem img="/smoothie.png" name="Smoothie" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={30} category={"drink"} />
+          <MenuItem img="/smoothie.png" name="Smoothie" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={30} category={"drink"} />
+          <MenuItem img="/smoothie.png" name="Smoothie" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={30} category={"drink"} />
+                
+        </div>        
       </main>
     </>
   )
