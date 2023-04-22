3d7900956fd1dba574d0487b02649557ec71c977
Deniz <denizhincal05@gmail.com>
2023-04-22 12:14:02 +0300

    Fix Sidebar Height Problem

--- paris-ghost-kicthen/src/components/navbar.jsx
+++ paris-ghost-kicthen/src/components/navbar.jsx
@@ -10,12 +10,7 @@ export default function Navbar() {
 
     return (
     <>
-        <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet" />
-        </Head>
-        <div className={`${sidebar ? "absolute mobileSidebar" : " -translate-x-[100vw]"} text-white font-semibold text-2xl  gap-y-5 flex flex-col bg-kitchen-green absolute h-screen w-[75vw] overflow-y-hidden z-50 p-5 transition-all duration-300`}>
+        <div className={`${sidebar ? "overflow-y-hidden mobileSidebar" : " -translate-x-[100vw]"} text-white font-semibold text-2xl z-50  gap-y-5 flex flex-col bg-kitchen-green absolute h-[100lvh] w-[75vw] overflow-y-hidden p-5 transition-all duration-300`}>
             <button className='w-fit' onClick={() => setSidebar(!sidebar)}>
                 <XMarkIcon className="w-10 -translate-x-2" />
             </button>
@@ -29,7 +24,7 @@ export default function Navbar() {
                 <button>Hakkımızda</button>
             </NextLink>
             </div>
-        <nav className='flex bg-kitchen-green p-4 sticky top-0 w-full'>
+        <nav className='flex bg-kitchen-green p-4 sticky top-0 w-full z-40'>
             <Bars3Icon onClick={() => setSidebar(true)} className='h-9 w-9 stroke-white' />
         </nav>
     </>
