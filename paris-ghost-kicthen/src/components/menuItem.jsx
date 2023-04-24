cd81b6edc31af28ce0cdffee1fa8eaa56b25688c
Deniz <denizhincal05@gmail.com>
2023-04-24 12:05:57 +0300

    Change Design of Menu

--- paris-ghost-kicthen/src/components/menuItem.jsx
+++ paris-ghost-kicthen/src/components/menuItem.jsx
@@ -2,20 +2,11 @@ import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function MenuItem({category, name, desc, price, img}) {
+export default function MenuItem({category, name, price}) {
    
     return (
         <div className='flex flex-col gap-y-1 items-center text-center'>
-            <div className='relative w-[100px] h-[100px]'>
-                <Image 
-                    src={img}
-                    fill
-                    className='object-contain'
-                    alt={category}
-                />
-            </div>
             <h3 className='font-semibold text-xl'>{name}</h3>
-            <p className='font-light text-lg'>{desc}</p>
             <h3 className='font-semibold text-xl'>{price} €</h3>
         </div>
   )
--- paris-ghost-kicthen/src/pages/index.jsx
+++ paris-ghost-kicthen/src/pages/index.jsx
@@ -13,17 +13,31 @@ export default function Home() {
       <main className='flex flex-col items-center px-6 py-3'>
         <Image src="/logo1.png" height={160} width={160} alt='Paris Ghost Kitchen'/>
         <h2 className='text-2xl font-semibold mt-6'>Menü</h2>
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-10'>Bowls</h3>
+        <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
+          <Image 
+            src="/bowls.jpg" 
+            fill
+            className='object-cover rounded-full'
+          />
+        </div>
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5'>Bowls</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
-          <MenuItem img="/soup1.png" name="Salmon Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={19.90} category={"soup"}/>
-          <MenuItem img="/soup1.png" name="Falafel Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={17.90} category={"soup"}/>
-          <MenuItem img="/soup1.png" name="Seafood ( Poke ) Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={19.90} category={"soup"}/>
-          <MenuItem img="/soup1.png" name="Veggie Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={17.90} category={"soup"}/>
-          <MenuItem img="/soup1.png" name="Black Forest ( Acai ) Bowl" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " price={18.90} category={"soup"}/>
+          <MenuItem  name="Salmon Bowl" price={19.90} category={"soup"}/>
+          <MenuItem  name="Falafel Bowl" price={17.90} category={"soup"}/>
+          <MenuItem  name="Seafood ( Poke ) Bowl"  price={19.90} category={"soup"}/>
+          <MenuItem  name="Veggie Bowl" price={17.90} category={"soup"}/>
+          <MenuItem  name="Black Forest ( Acai ) Bowl" price={18.90} category={"soup"}/>
         
-         </div>      
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-10'>Bagels</h3>
+        </div>
+        <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
+          <Image 
+            src="/bagels.jpg" 
+            fill
+            className='object-cover rounded-full'
+          />
+        </div>      
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5'>Bagels</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
           <MenuItem img="/bagel.png" name="Egg Avocado Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={18.90} category={"bagel"} />
@@ -33,7 +47,14 @@ export default function Home() {
           <MenuItem img="/bagel.png" name="Bruschetta Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={17.90} category={"bagel"} />
         
         </div>
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-10'>Wraps</h3>
+        <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
+          <Image 
+            src="/wraps.jpeg" 
+            fill
+            className='object-cover rounded-full'
+          />
+        </div>      
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5'>Wraps</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
           <MenuItem img="/wrap.png" name="Falafel Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={17.90} category={"wrap"} />
@@ -42,7 +63,14 @@ export default function Home() {
           <MenuItem img="/wrap.png" name="King Crab Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={24.90} category={"wrap"} />
           <MenuItem img="/wrap.png" name="Katz’s Deli Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={24.90} category={"wrap"} />      
         </div>
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-10 text-center'>Something to Drink</h3>
+        <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
+          <Image 
+            src="/drinks.jpg" 
+            fill
+            className='object-cover rounded-full'
+          />
+        </div>      
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5 text-center'>Something to Drink</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
           <MenuItem img="/smoothie.png" name="Smoothie" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={6.90} category={"drink"} />
