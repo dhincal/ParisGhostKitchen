268a1e4a6ef30b9b11cc743679c07f59b282c01e
Deniz <denizhincal05@gmail.com>
2023-04-26 22:12:27 +0300

    Add Map to Website

--- paris-ghost-kicthen/src/pages/hakkimizda.jsx
+++ paris-ghost-kicthen/src/pages/hakkimizda.jsx
@@ -45,6 +45,7 @@ export default function AboutUs() {
                         <div className='flex gap-x-2 items-center'>
                             <PhoneIcon className='h-5 w-5'/><p>0 (532) 290 25 99</p>
                         </div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d99930.12436793988!2d2.4911776616984587!3d48.83979808440579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e672ad2d1967f9%3A0xf16014d390b3f025!2sAquatic%20Center%20Dome%20Vincennes!5e0!3m2!1str!2str!4v1682536032901!5m2!1str!2str" className='w-full rounded-xl border-2 border-kitchen-green' height="350" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     
                     <div className='w-full h-[2px] bg-kitchen-green mt-5' />
                     <p className='text-kitchen-dark text-xs text-center'>Telif Hakkı © 2023 Paris Ghost Kitchen - Tüm Hakları Saklıdır.</p>
