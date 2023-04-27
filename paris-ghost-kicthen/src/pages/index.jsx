4e85e190eaed0d7235fd45398f6b00dfb4094a3e
Deniz <hincal32619@yazilimlisesi.k12.tr>
2023-04-28 00:01:59 +0300

    Change Content of Menu

--- paris-ghost-kicthen/src/pages/index.jsx
+++ paris-ghost-kicthen/src/pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
       <Navbar />
       <main className='flex flex-col items-center px-6 py-3 bg-kitchen-orange/5'>
         <Image src="/logo1.png" height={160} width={160} alt='Paris Ghost Kitchen'/>
-        <h2 className='text-2xl font-semibold mt-6'>Menü</h2>
+        <h2 className='text-2xl font-semibold mt-6'>Menu</h2>
         <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
           <Image 
             src="/bowls.jpg" 
@@ -22,14 +22,14 @@ export default function Home() {
             alt='bowls'
           />
         </div>
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-5'>Bowls</h3>
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5 uppercase'>Bowls / Boules</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
-          <MenuItem  name="Salmon Bowl" price={19.90} category={"soup"}/>
-          <MenuItem  name="Falafel Bowl" price={17.90} category={"soup"}/>
-          <MenuItem  name="Seafood ( Poke ) Bowl"  price={19.90} category={"soup"}/>
-          <MenuItem  name="Veggie Bowl" price={17.90} category={"soup"}/>
-          <MenuItem  name="Black Forest ( Acai ) Bowl" price={18.90} category={"soup"}/>
+          <MenuItem  name="Salmon Bowl / Bol De Saumon" price={19.90} category={"soup"}/>
+          <MenuItem  name="Falafel Bowl / Bol Falafel" price={17.90} category={"soup"}/>
+          <MenuItem  name="Seafood ( Poke ) Bowl / Bol de Fruits De Mer"  price={19.90} category={"soup"}/>
+          <MenuItem  name="Veggie Bowl / Bol De Légumes" price={17.90} category={"soup"}/>
+          <MenuItem  name="Black Forest ( Acai ) Bowl / Bol Forêt Noire" price={18.90} category={"soup"}/>
         
         </div>
         <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
@@ -40,14 +40,14 @@ export default function Home() {
             alt='bagels'
           />
         </div>      
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-5'>Bagels</h3>
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5 uppercase'>Bagels</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
-          <MenuItem img="/bagel.png" name="Egg Avocado Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={18.90} category={"bagel"} />
-          <MenuItem img="/bagel.png" name="Cheese Smoked Salmon Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={19.90} category={"bagel"} />
-          <MenuItem img="/bagel.png" name="Mediterranean Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={17.90} category={"bagel"} />
-          <MenuItem img="/bagel.png" name="Smoked Ribeye Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={19.90} category={"bagel"} />
-          <MenuItem img="/bagel.png" name="Bruschetta Bagel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={17.90} category={"bagel"} />
+          <MenuItem name="Egg Avocado Bagel / Bagel Oeuf Avocat" price={18.90} category={"bagel"} />
+          <MenuItem name="Cheese Smoked Salmon Bagel / Begel Au Saumon Fumé Au Fromage" price={19.90} category={"bagel"} />
+          <MenuItem name="Mediterranean Bagel / Bagel Méditerranéen" price={17.90} category={"bagel"} />
+          <MenuItem name="Smoked Ribeye Bagel / Bagel De Faux-Filet Fumé" price={19.90} category={"bagel"} />
+          <MenuItem name="Bruschetta Bagel / Bagels à La Bruschetta" price={17.90} category={"bagel"} />
         
         </div>
         <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
@@ -58,14 +58,14 @@ export default function Home() {
             alt='wraps'
           />
         </div>      
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-5'>Wraps</h3>
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5 uppercase'>Wraps / ENVELOPPES</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
-          <MenuItem img="/wrap.png" name="Falafel Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={17.90} category={"wrap"} />
-          <MenuItem img="/wrap.png" name="Burrito Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={19.90} category={"wrap"} />
-          <MenuItem img="/wrap.png" name="Mediterranean Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={17.90} category={"wrap"} />
-          <MenuItem img="/wrap.png" name="King Crab Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={24.90} category={"wrap"} />
-          <MenuItem img="/wrap.png" name="Katz’s Deli Wrap" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={24.90} category={"wrap"} />      
+          <MenuItem name="Falafel Wrap / Wrap Aux Falafels" price={17.90} category={"wrap"} />
+          <MenuItem name="Burrito Wrap / Wrap Burritos" price={19.90} category={"wrap"} />
+          <MenuItem name="Mediterranean Wrap / Wrap Méditerranéen" price={17.90} category={"wrap"} />
+          <MenuItem name="King Crab Wrap / Wrap Au Crabe Royal" price={24.90} category={"wrap"} />
+          <MenuItem name="Katz’s Deli Wrap / Deli Wrap De Katz’s" price={24.90} category={"wrap"} />      
         </div>
         <div className='w-[250px] h-[250px] rounded-full relative mt-10'>
           <Image 
@@ -75,14 +75,14 @@ export default function Home() {
             alt='drinks'
           />
         </div>      
-        <h3 className='text-kitchen-green text-2xl font-semibold mt-5 text-center'>Something to Drink</h3>
+        <h3 className='text-kitchen-green text-2xl font-semibold mt-5 text-center uppercase'>Something to Drink / QUELQUE CHOSE À BOIRE</h3>
         <div className='w-full h-[2px] bg-kitchen-green mt-2' />        
         <div className='flex flex-col items-center mt-10 gap-y-10'>
-          <MenuItem img="/smoothie.png" name="Smoothie" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={6.90} category={"drink"} />
-          <MenuItem img="/smoothie.png" name="Fresh Juice" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={6.90} category={"drink"} />
-          <MenuItem img="/smoothie.png" name="Basil Mint Ayran" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={4.90} category={"drink"} />
-          <MenuItem img="/smoothie.png" name="Perrier" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={4.90} category={"drink"} />
-          <MenuItem img="/smoothie.png" name="Evian" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit" price={4.90} category={"drink"} />
+          <MenuItem name="Smoothie" price={6.90} category={"drink"} />
+          <MenuItem name="Fresh Juice / Jus Frais" price={6.90} category={"drink"} />
+          <MenuItem name="Basil Mint Ayran / Basilic Menthe Ayran" price={4.90} category={"drink"} />
+          <MenuItem name="Perrier" price={4.90} category={"drink"} />
+          <MenuItem name="Évian" price={4.90} category={"drink"} />
         </div>              
       </main>
       <Footer />
